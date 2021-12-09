bf29fdedc15d7897179085159fa3198f57c6938e
adairconlin <adairconlin@gmail.com>
2021-12-09 13:00:35 -0500

    Complete styling

--- src/page-template.js
+++ src/page-template.js
@@ -1,74 +1,79 @@
 function generateIntern(intern) {
     const internHTML = intern.map(({ name, id, email, school }) => {
         return `<div class="card">
-                    <h1>Engineer</h1>
-                    <div>
-                        <h1>Name: ${name}</h1>
+                    <div class="title-section">
+                        <h1 class="title myFont medium">Intern</h1>
                     </div>
-                    <div>
-                        <h3>ID: ${id}</h3>
-                    </div>
-                    <div>
-                    <h3>Email: <a href="mailto: ${email}">${email}</a></h3>
-                    </div>
-                    <div>
-                        <h3>School: ${school}</h3>
+                    <div class="employee-info">
+                        <div>
+                            <h1 class="myFont bold">${name}</h1>
+                        </div>
+                        <div>
+                            <h3 class="myFont">ID: ${id}</h3>
+                        </div>
+                        <div>
+                        <h3 class="myFont">Email: <a href="mailto: ${email}">${email}</a></h3>
+                        </div>
+                        <div>
+                            <h3 class="myFont">School: ${school}</h3>
+                        </div>
                     </div>
                 </div>`;
     });
 
-    return `<section class="role-section">
-                ${internHTML.join('')}
-            </section>`;
+    return `${internHTML.join('')}`;
 };
 
 function generateEngineer(engineer) {
     const engineerHTML = engineer.map(({ name, id, email, github }) => {
         return `<div class="card">
-                    <h1>Engineer</h1>
-                    <div>
-                        <h1>Name: ${name}</h1>
+                    <div class="title-section">
+                        <h1 class="title myFont medium">Engineer</h1>
                     </div>
-                    <div>
-                        <h3>ID: ${id}</h3>
-                    </div>
-                    <div>
-                        <h3>Email: <a href="mailto: ${email}">${email}</a></h3>
-                    </div>
-                    <div>
-                        <h3><a href="https://github.com/${github}/">GitHub</a></h3>
+                    <div class="employee-info">
+                        <div>
+                            <h1 class="myFont bold">${name}</h1>
+                        </div>
+                        <div>
+                            <h3 class="myFont">ID: ${id}</h3>
+                        </div>
+                        <div>
+                            <h3 class="myFont">Email: <a href="mailto: ${email}">${email}</a></h3>
+                        </div>
+                        <div>
+                            <h3 class="myFont"><a href="https://github.com/${github}/">GitHub</a></h3>
+                        </div>
                     </div>
                 </div>`;
     });
 
-    return `<section class="role-section">
-                ${engineerHTML.join('')}
-            </section>`;
+    return `${engineerHTML.join('')}`
 };
 
 function generateManager(manager) {
     const managerHTML = manager.map(({ name, id, email, officeNumber }) => {
         return `<div class="card">
-                    <h1>Manager</h1>
-                    <div>
-                        <h1>Name: ${name}</h1>
+                    <div class="title-section">
+                        <h1 class="title myFont medium">Manager</h1>
                     </div>
-                    <div>
-                        <h3>ID: ${id}</h3>
-                    </div>
-                    <div>
-                    <h3>Email: <a href="mailto: ${email}">${email}</a></h3>
-                    </div>
-                    <div>
-                        <h3>Office Number: ${officeNumber}</h3>
+                    <div class="employee-info">
+                        <div>
+                            <h1 class="myFont bold">${name}</h1>
+                        </div>
+                        <div>
+                            <h3 class="myFont">ID: ${id}</h3>
+                        </div>
+                        <div>
+                        <h3 class="myFont">Email: <a href="mailto: ${email}">${email}</a></h3>
+                        </div>
+                        <div>
+                            <h3 class="myFont">Office Number: ${officeNumber}</h3>
+                        </div>
                     </div>
                 </div>`;
     });
 
-    return `<section class="role-section">
-                ${managerHTML.join('')}
-            </section>
-            `;
+    return `${managerHTML.join('')}`;
 };
 
 function generatePage(data) {
@@ -80,12 +85,13 @@ function generatePage(data) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Team Profile Generator</title>
+        <link rel="stylesheet" href="https://use.typekit.net/fwi4mis.css">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <header>
             <div>
-                <h1>Team Profile</h1>
+                <h1 class="myFont">Web Development Team</h1>
             </div>
         </header>
 
@@ -94,6 +100,10 @@ function generatePage(data) {
             ${generateEngineer(data.engineers)}
             ${generateIntern(data.interns)}
         </main>
+
+        <footer>
+            <h3 class="myFont">&copy; ${new Date().getFullYear()} by Adair Conlin</h3>
+        </footer>
     </body>
 </html>
     `
